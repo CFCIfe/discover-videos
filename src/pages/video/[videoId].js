ec0daa7ca111189e8a8c390e11b3ab44533e7f12
cfcife <aboludepeter@gmail.com>
2023-12-26 23:57:16 +0100

    Add video details to modal

--- src/pages/video/[videoId].js
+++ src/pages/video/[videoId].js
@@ -2,10 +2,21 @@ import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
 
+import classnames from "classnames";
+
 Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
+  const video = {
+    title: "Dummy Title",
+    publishTime: "2022-01-01",
+    description: "This is a dummy video",
+    channelTitle: "Dummy Channel",
+    viewCount: 1000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
 
   return (
     <div className={styles.container}>
@@ -16,7 +27,34 @@ const Video = () => {
         className={styles.modal}
         overlayClassName={styles.overlay}
       >
-        <div>Modal Body </div>
+        <iframe
+          id="ytplayer"
+          className={styles.videoPlayer}
+          type="text/html"
+          width="640"
+          height="360"
+          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
+        ></iframe>
+
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={classnames(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={classnames(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
