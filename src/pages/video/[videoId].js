fccdf1e20516af5aa5a8c04b620390ee68bf1e94
cfcife <aboludepeter@gmail.com>
2023-12-31 00:58:14 +0100

    Add NavBar component to Video page

--- src/pages/video/[videoId].js
+++ src/pages/video/[videoId].js
@@ -4,6 +4,7 @@ import styles from "../../styles/Video.module.css";
 import { getYoutubeVideoById } from "../../../lib/videos";
 
 import classnames from "classnames";
+import NavBar from "../../../components/nav/navbar";
 
 Modal.setAppElement("#__next");
 
@@ -48,42 +49,48 @@ const Video = ({ video }) => {
 
   return (
     <div className={styles.container}>
-      <Modal
-        isOpen={true}
-        onRequestClose={() => router.back()}
-        contentLabel="Watch the video"
-        className={styles.modal}
-        overlayClassName={styles.overlay}
-      >
-        <iframe
-          id="ytplayer"
-          className={styles.videoPlayer}
-          type="text/html"
-          width="640"
-          height="360"
-          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
-        ></iframe>
+      <NavBar>
+        <Modal
+          isOpen={true}
+          onRequestClose={() => router.back()}
+          contentLabel="Watch the video"
+          className={styles.modal}
+          overlayClassName={styles.overlay}
+        >
+          <iframe
+            id="ytplayer"
+            className={styles.videoPlayer}
+            type="text/html"
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
+          ></iframe>
 
-        <div className={styles.modalBody}>
-          <div className={styles.modalBodyContent}>
-            <div className={styles.col1}>
-              <p className={styles.publishTime}>{publishTime}</p>
-              <p className={styles.title}>{title}</p>
-              <p className={styles.description}>{description}</p>
-            </div>
-            <div className={styles.col2}>
-              <p className={classnames(styles.subText, styles.subTextWrapper)}>
-                <span className={styles.textColor}>Cast: </span>
-                <span className={styles.channelTitle}>{channelTitle}</span>
-              </p>
-              <p className={classnames(styles.subText, styles.subTextWrapper)}>
-                <span className={styles.textColor}>View Count: </span>
-                <span className={styles.channelTitle}>{viewCount}</span>
-              </p>
+          <div className={styles.modalBody}>
+            <div className={styles.modalBodyContent}>
+              <div className={styles.col1}>
+                <p className={styles.publishTime}>{publishTime}</p>
+                <p className={styles.title}>{title}</p>
+                <p className={styles.description}>{description}</p>
+              </div>
+              <div className={styles.col2}>
+                <p
+                  className={classnames(styles.subText, styles.subTextWrapper)}
+                >
+                  <span className={styles.textColor}>Cast: </span>
+                  <span className={styles.channelTitle}>{channelTitle}</span>
+                </p>
+                <p
+                  className={classnames(styles.subText, styles.subTextWrapper)}
+                >
+                  <span className={styles.textColor}>View Count: </span>
+                  <span className={styles.channelTitle}>{viewCount}</span>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </Modal>
+        </Modal>
+      </NavBar>
     </div>
   );
 };
