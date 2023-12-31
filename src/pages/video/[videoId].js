c0de5cb3bb23ac3af9ca96677362c506a0f58690
cfcife <aboludepeter@gmail.com>
2023-12-31 01:04:29 +0100

    Refactor video page layout

--- src/pages/video/[videoId].js
+++ src/pages/video/[videoId].js
@@ -49,48 +49,43 @@ const Video = ({ video }) => {
 
   return (
     <div className={styles.container}>
-      <NavBar>
-        <Modal
-          isOpen={true}
-          onRequestClose={() => router.back()}
-          contentLabel="Watch the video"
-          className={styles.modal}
-          overlayClassName={styles.overlay}
-        >
-          <iframe
-            id="ytplayer"
-            className={styles.videoPlayer}
-            type="text/html"
-            width="640"
-            height="360"
-            src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
-          ></iframe>
+      <NavBar />
+      <Modal
+        isOpen={true}
+        onRequestClose={() => router.back()}
+        contentLabel="Watch the video"
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+      >
+        <iframe
+          id="ytplayer"
+          className={styles.videoPlayer}
+          type="text/html"
+          width="640"
+          height="360"
+          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
+        ></iframe>
 
-          <div className={styles.modalBody}>
-            <div className={styles.modalBodyContent}>
-              <div className={styles.col1}>
-                <p className={styles.publishTime}>{publishTime}</p>
-                <p className={styles.title}>{title}</p>
-                <p className={styles.description}>{description}</p>
-              </div>
-              <div className={styles.col2}>
-                <p
-                  className={classnames(styles.subText, styles.subTextWrapper)}
-                >
-                  <span className={styles.textColor}>Cast: </span>
-                  <span className={styles.channelTitle}>{channelTitle}</span>
-                </p>
-                <p
-                  className={classnames(styles.subText, styles.subTextWrapper)}
-                >
-                  <span className={styles.textColor}>View Count: </span>
-                  <span className={styles.channelTitle}>{viewCount}</span>
-                </p>
-              </div>
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={classnames(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={classnames(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
             </div>
           </div>
-        </Modal>
-      </NavBar>
+        </div>
+      </Modal>
     </div>
   );
 };
